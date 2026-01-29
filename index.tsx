37b5a3c04f974b5c6c043bc27e068dd6f7c1d3c9
coisasane01-droid <coisasane01@gmail.com>
2026-01-29 20:11:29 +0000

    fix manifest.json for netlify

--- index.tsx
+++ index.tsx
@@ -2,6 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // Registro do Service Worker e solicitação de permissão de notificação
 if ('serviceWorker' in navigator) {
